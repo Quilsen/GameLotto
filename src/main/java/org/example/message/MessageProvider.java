1c8ce8efe97fe25248f2d64c8d8c15d3eaba086f
tomaszmlodzianowski <t.mlodzianowski@gmail.com>
2023-11-13 15:34:02 +0100

    messages to provide while playing

--- src/main/java/org/example/message/MessageProvider.java
+++ src/main/java/org/example/message/MessageProvider.java
@@ -1,4 +1,9 @@
 package org.example.message;
 
 public class MessageProvider {
+    public static final String WELCOME_MSG = "Welcome in Lotto Game.";
+    public static final String PROVIDE_NUMBERS = "Please provide %d numbers.";
+    public static final String READ_NUMER = "%d NUMBER: ";
+    public static final String INVALID_NUMBER = "Please, give number from %d to %d.";
+    public static final String RESULT_INFO = "You get %d hits. Winning numbers: %s vs Your numbers: %S .";
 }
