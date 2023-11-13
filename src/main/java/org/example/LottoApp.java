b49005d6a2aa076e40676fdae87eb19f9882d17c
tomaszmlodzianowski <t.mlodzianowski@gmail.com>
2023-11-13 15:34:59 +0100

    setup of game to run

--- src/main/java/org/example/LottoApp.java
+++ src/main/java/org/example/LottoApp.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import org.example.calculator.HitCounter;
+import org.example.input.InputProvider;
+import org.example.output.ConsolePrinter;
+import org.example.random.RandomGenerator;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class LottoApp {
+    public static void main(String[] args) {
+        LottoGame lottoGame = getLottoGame();
+        lottoGame.run();
+    }
+
+    private static LottoGame getLottoGame() {
+        HitCounter hitCounter = new HitCounter();
+        InputProvider inputProvider = new InputProvider();
+        ConsolePrinter consolePrinter = new ConsolePrinter();
+        RandomGenerator randomGenerator = new RandomGenerator();
+        Scanner scanner = new Scanner(System.in);
+        LottoGame lottoGame = new LottoGame(inputProvider, hitCounter, randomGenerator, consolePrinter, scanner);
+        return lottoGame;
+    }
 }
