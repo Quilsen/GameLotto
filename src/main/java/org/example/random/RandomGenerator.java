2a282a0f842bf83468814c66ea5824748b52b588
tomaszmlodzianowski <t.mlodzianowski@gmail.com>
2023-11-13 14:24:25 +0100

    RandomGenerator.java ready for tests

--- src/main/java/org/example/random/RandomGenerator.java
+++ src/main/java/org/example/random/RandomGenerator.java
@@ -1,4 +1,21 @@
 package org.example.random;
 
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static org.example.LottoGame.LOTTO_NR_COUNT;
+import static org.example.LottoGame.UPPER_BOUND;
+
 public class RandomGenerator {
+
+    public Set<Integer> getRandomNumbers(){
+        Random random = new Random();
+        Set<Integer> randomNumbers = new LinkedHashSet<>();
+        while (randomNumbers.size() < LOTTO_NR_COUNT){
+            int randomNumber = random.nextInt(UPPER_BOUND + 1);
+            randomNumbers.add(randomNumber);
+        }
+        return randomNumbers;
+    }
 }
