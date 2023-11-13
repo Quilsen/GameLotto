9e2f9f212b8258f2e6b8c3f40ee47d4b81a845b5
tomaszmlodzianowski <t.mlodzianowski@gmail.com>
2023-11-13 15:32:58 +0100

    hit counter

---
+++ src/main/java/org/example/calculator/HitCounter.java
@@ -0,0 +1,17 @@
+package org.example.calculator;
+
+import java.util.Set;
+
+public class HitCounter {
+
+    public int getHitCount(Set<Integer> userNumbers, Set<Integer> randomNumbers){
+        int hitCount = 0;
+        for (Integer randomNumber : randomNumbers) {
+            for (Integer userNumber : userNumbers) {
+                if (randomNumber.equals(userNumber))
+                    hitCount++;
+            }
+        }
+        return hitCount;
+    }
+}
